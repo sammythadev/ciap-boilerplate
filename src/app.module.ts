8447b9655b1df8a6c5c6b6cbe338d56f9627a95e
munachi <munachielvis815@gmail.com>
2026-04-07 20:18:40 +0100

    chore: fixed swc compile issue

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { HealthModule } from '@modules/health/health.module';
 import { UsersModule } from '@modules/users/users.module';
 import { DatabaseModule } from '@database/database.module';
 import { SeedModule } from '@database/seeds/seed.module';
+import { CommonModule } from '@common/common.module';
 
 @Module({
-  imports: [DatabaseModule, SeedModule, HealthModule, UsersModule],
+  imports: [DatabaseModule, SeedModule, HealthModule, UsersModule,CommonModule],
   controllers: [AppController],
   providers: [AppService],
 })
